c436e6943a219470ecdc1e6e1bba7a7ab9bddced
NikitaL123 <m3yui100@gmail.com>
2026-04-17 09:23:44 +0500

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,5 +1,5 @@
 import os
 import whisper
-import streamlit as st
+import streamlit as st #на самом деле классная штука, я бы поизучал ее побольше, можно сделать интерфейс в пару строчек (но главное разместить эти строчки правильно иначе будет плохо)
 from openai import OpenAI
 from typing import Dict, Any
